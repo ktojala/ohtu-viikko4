a64cfd1abafb3ddd781d250848b44d948bf76a34
Kari Ojala <kari.ojala@helsinki.fi>
2018-11-25 00:25:00 +0200

    v4 t5

--- VerkkokauppaSpring/src/test/java/ohtu/verkkokauppa/KauppaTest.java
+++ VerkkokauppaSpring/src/test/java/ohtu/verkkokauppa/KauppaTest.java
@@ -7,22 +7,28 @@ import static org.mockito.Mockito.*;
 
 public class KauppaTest {
     
+        Pankki pankki;
+        Viitegeneraattori viite;
+        Varasto varasto;
+        Kauppa k;
+
+    @Before
+    public void Setup() {
+    // luodaan mock-oliot
+        pankki = mock(Pankki.class);
+        viite = mock(Viitegeneraattori.class);
+        varasto = mock(Varasto.class);
+        k = new Kauppa(varasto, pankki, viite);
+    }
+
     @Test
     public void ostoksenPaaytyttyaPankinMetodiaTilisiirtoKutsutaan() {
-    // luodaan ensin mock-oliot
-        Pankki pankki = mock(Pankki.class);
-    
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+
     // määritellään että viitegeneraattori palauttaa viitten 42
         when(viite.uusi()).thenReturn(42);
-
-        Varasto varasto = mock(Varasto.class);
     // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
         when(varasto.saldo(1)).thenReturn(10); 
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
-
-    // sitten testattava kauppa 
-        Kauppa k = new Kauppa(varasto, pankki, viite);              
 
     // tehdään ostokset
         k.aloitaAsiointi();
@@ -36,20 +42,13 @@ public class KauppaTest {
     
     @Test
     public void ostoksenPaatyttyaPankinMetodiaTilisiirtoKutsutaanJaOikeatArvot() {
-    // luodaan ensin mock-oliot
-        Pankki pankki = mock(Pankki.class);
-    
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+
     // määritellään että viitegeneraattori palauttaa viitten 42
         when(viite.uusi()).thenReturn(42);
 
-        Varasto varasto = mock(Varasto.class);
     // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
         when(varasto.saldo(1)).thenReturn(10); 
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
-
-    // sitten testattava kauppa 
-        Kauppa k = new Kauppa(varasto, pankki, viite);              
 
     // tehdään ostokset
         k.aloitaAsiointi();
@@ -63,23 +62,16 @@ public class KauppaTest {
     
     @Test
     public void kahdenEriTuotteenOstonJalkeenOikeatArvot() {
-    // luodaan ensin mock-oliot
-        Pankki pankki = mock(Pankki.class);
-    
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+
     // määritellään että viitegeneraattori palauttaa viitten 42
         when(viite.uusi()).thenReturn(42);
 
-        Varasto varasto = mock(Varasto.class);
     // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
         when(varasto.saldo(1)).thenReturn(10); 
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
     // määritellään että tuote numero 2 on akhvi jonka hinta on 2 ja saldo 8
         when(varasto.saldo(2)).thenReturn(8); 
-        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "kahvi", 2));       
-
-    // sitten testattava kauppa 
-        Kauppa k = new Kauppa(varasto, pankki, viite);              
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "kahvi", 2));
 
     // tehdään ostokset
         k.aloitaAsiointi();
@@ -94,20 +86,13 @@ public class KauppaTest {
     
     @Test
     public void kahdenSamanTuotteenOstonJalkeenOikeatArvot() {
-    // luodaan ensin mock-oliot
-        Pankki pankki = mock(Pankki.class);
-    
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+
     // määritellään että viitegeneraattori palauttaa viitten 42
         when(viite.uusi()).thenReturn(42);
 
-        Varasto varasto = mock(Varasto.class);
     // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
         when(varasto.saldo(1)).thenReturn(10); 
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
-
-    // sitten testattava kauppa 
-        Kauppa k = new Kauppa(varasto, pankki, viite);              
 
     // tehdään ostokset
         k.aloitaAsiointi();
@@ -122,23 +107,16 @@ public class KauppaTest {
     
     @Test
     public void kahdenEriTuotteenJoistaToinenLoppuOstonJalkeenOikeatArvot() {
-    // luodaan ensin mock-oliot
-        Pankki pankki = mock(Pankki.class);
-    
-        Viitegeneraattori viite = mock(Viitegeneraattori.class);
+
     // määritellään että viitegeneraattori palauttaa viitten 42
         when(viite.uusi()).thenReturn(42);
 
-        Varasto varasto = mock(Varasto.class);
     // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
         when(varasto.saldo(1)).thenReturn(10); 
         when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
     // määritellään että tuote numero 2 on akhvi jonka hinta on 2 ja saldo 0
         when(varasto.saldo(2)).thenReturn(8); 
-        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "kahvi", 0));       
-
-    // sitten testattava kauppa 
-        Kauppa k = new Kauppa(varasto, pankki, viite);              
+        when(varasto.haeTuote(2)).thenReturn(new Tuote(2, "kahvi", 0));
 
     // tehdään ostokset
         k.aloitaAsiointi();
@@ -149,6 +127,70 @@ public class KauppaTest {
     // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
     //     tilisiirto(String nimi, int viitenumero, String tililta, String tilille, int summa) {
         verify(pankki).tilisiirto(eq("pekka"), anyInt(), eq("12345"), anyString(),eq(5));   
+    }
+
+    @Test
+    public void aloitaAsiointiNollaaEdellisenOstoksenTiedot() {
+
+    // määritellään että viitegeneraattori palauttaa viitten 43
+        when(viite.uusi()).thenReturn(43);
+
+    // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+    // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.aloitaAsiointi();   // uusi asiointi, vanha jäi silleen
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("pekka", "12345");
+
+    // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+    //     tilisiirto(String nimi, int viitenumero, String tililta, String tilille, int summa) {
+        verify(pankki).tilisiirto(eq("pekka"), eq(43), eq("12345"), anyString(),eq(5));
+    }
+
+    @Test
+    public void tarkistaUusiViitenumeroUudestaMaksutapahtumasta() {
+
+        when(viite.uusi()).thenReturn(42).thenReturn(43);
+
+    // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 15
+        when(varasto.saldo(1)).thenReturn(15).thenReturn(14);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+    // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("jukka", "12344");
+
+        k.aloitaAsiointi();   // uusi asiointi
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.tilimaksu("jukka", "12344");
+
+        verify(viite,times(2)).uusi();
+    }
+
+    @Test
+    public void poistaTuoteKorista() {
+
+        when(viite.uusi()).thenReturn(43);
+
+    // määritellään että tuote numero 1 on maito jonka hinta on 5 ja saldo 10
+        when(varasto.saldo(1)).thenReturn(10);
+        when(varasto.haeTuote(1)).thenReturn(new Tuote(1, "maito", 5));
+
+    // tehdään ostokset
+        k.aloitaAsiointi();
+        k.lisaaKoriin(1);     // ostetaan tuotetta numero 1 eli maitoa
+        k.poistaKorista(1);
+        k.tilimaksu("jukka", "12344");
+
+    // sitten suoritetaan varmistus, että pankin metodia tilisiirto on kutsuttu
+    //     tilisiirto(String nimi, int viitenumero, String tililta, String tilille, int summa) {
+        verify(pankki).tilisiirto(eq("jukka"), anyInt(), eq("12344"), anyString(), eq(0));
+    //
     }
     
 }
